000b058046ce2df51153995a4e0ac6addf93adb9
n3vrax <n3vrax@gmail.com>
2017-04-07 19:09:26 +0300

    replace dot-frontend strings with frontend

--- src/User/src/Entity/UserEntity.php
+++ src/User/src/Entity/UserEntity.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @see https://github.com/dotkernel/dot-frontend/ for the canonical source repository
+ * @see https://github.com/dotkernel/frontend/ for the canonical source repository
  * @copyright Copyright (c) 2017 Apidemia (https://www.apidemia.com)
- * @license https://github.com/dotkernel/dot-frontend/blob/master/LICENSE.md MIT License
+ * @license https://github.com/dotkernel/frontend/blob/master/LICENSE.md MIT License
  */
 
 declare(strict_types = 1);
