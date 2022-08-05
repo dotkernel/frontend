8ef0d89847654f3142ee167819cdcdf844d787e0
marioradu <magdamarior@gmail.com>
2022-08-05 16:43:53 +0300

    doctrine & phinx migrations & doctrine diff command

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,56 +1,13 @@
 <?php
 
-use Doctrine\DBAL\DriverManager;
-use Doctrine\Migrations\Configuration\Configuration;
-use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
-use Doctrine\Migrations\Configuration\Migration\ExistingConfiguration;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\PhpFile;
 use Doctrine\Migrations\DependencyFactory;
-use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
-use Doctrine\Migrations\Tools\Console\Command;
-use Symfony\Component\Console\Application;
 
 $container = require __DIR__ . '/container.php';
-$migrations = require __DIR__ . '/migrations.php';
-$config = $migrations['doctrine_configuration'];
 
-try {
-    $connection = DriverManager::getConnection($container->get('config')['databases']['default'] ?? null);
-} catch (Exception $exception) {
-    exit($exception->getMessage());
-}
+$config = new PhpFile('config/doctrine_migrations.php');
 
-$configuration = new Configuration();
-$configuration->addMigrationsDirectory($config['migrations_directory_namespace'], $config['migrations_directory']);
-$configuration->setAllOrNothing($config['all_or_nothing']);
-$configuration->setCheckDatabasePlatform($config['check_database_platform']);
-$configuration->setConnectionName($config['connection_name']);
+$entityManager = $container->get('doctrine.entity_manager.orm_default');
 
-$storageConfiguration = new TableMetadataStorageConfiguration();
-$storageConfiguration->setTableName($config['table_name']);
-$storageConfiguration->setVersionColumnName($config['column_name']);
-$storageConfiguration->setVersionColumnLength($config['column_length']);
-$storageConfiguration->setExecutedAtColumnName($config['executed_at_column_name']);
-
-$configuration->setMetadataStorageConfiguration($storageConfiguration);
-
-$dependencyFactory = DependencyFactory::fromConnection(
-    new ExistingConfiguration($configuration),
-    new ExistingConnection($connection)
-);
-$cli = new Application('Doctrine Migrations');
-$cli->setCatchExceptions(true);
-
-$cli->addCommands(array(
-    new Command\DumpSchemaCommand($dependencyFactory),
-    new Command\ExecuteCommand($dependencyFactory),
-    new Command\GenerateCommand($dependencyFactory),
-    new Command\LatestCommand($dependencyFactory),
-    new Command\ListCommand($dependencyFactory),
-    new Command\MigrateCommand($dependencyFactory),
-    new Command\RollupCommand($dependencyFactory),
-    new Command\StatusCommand($dependencyFactory),
-    new Command\SyncMetadataCommand($dependencyFactory),
-    new Command\VersionCommand($dependencyFactory),
-));
-
-$cli->run();
+return DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
