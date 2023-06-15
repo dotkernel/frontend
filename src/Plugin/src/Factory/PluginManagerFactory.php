fa862c952462b5189096ccaa0fc009dc7fcb8c7c
MarioRadu <magda_marior@yahoo.com>
2023-06-15 12:18:52 +0300

    bug fix

--- src/Plugin/src/Factory/PluginManagerFactory.php
+++ src/Plugin/src/Factory/PluginManagerFactory.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Frontend\Plugin\Factory;
 
+use Dot\Controller\Plugin\UrlHelperPlugin;
 use Frontend\Plugin\PluginManager;
 use Frontend\Plugin\TemplatePlugin;
+use Mezzio\Helper\UrlHelper;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
@@ -28,6 +30,12 @@ class PluginManagerFactory
         $pluginManager = new PluginManager($container, $container->get('config')['dot_controller']['plugin_manager']);
 
         //register the built-in plugins, if the required component is present
+        if ($container->has(UrlHelper::class)) {
+            $pluginManager->setFactory('url', function (ContainerInterface $container) {
+                return new UrlHelperPlugin($container->get(UrlHelper::class));
+            });
+        }
+
         if ($container->has(TemplateRendererInterface::class)) {
             $pluginManager->setFactory('template', function (ContainerInterface $container) {
                 return new TemplatePlugin($container->get(TemplateRendererInterface::class));
