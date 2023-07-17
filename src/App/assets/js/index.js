eedaf12f4d82891b2dfe0136541b06a83c537eca
Mada <mada@rospace.com>
2023-07-17 20:41:40 +0300

    update to boostrap V5

--- src/App/assets/js/index.js
+++ src/App/assets/js/index.js
@@ -2,7 +2,6 @@
 try {
     window.$ = window.jQuery = require('jquery');
     require('bootstrap');
-    require('bootstrap-fileinput');
     require('bootstrap-slider');
     require('jquery-mousewheel');
 
