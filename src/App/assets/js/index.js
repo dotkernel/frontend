7dfda6962e58b4dac2aa629cb827e06f4c74841f
Mada <mada@rospace.com>
2023-07-27 15:54:52 +0300

    bootstrap-fileinput

--- src/App/assets/js/index.js
+++ src/App/assets/js/index.js
@@ -2,6 +2,7 @@
 try {
     window.$ = window.jQuery = require('jquery');
     require('bootstrap');
+    require('bootstrap-fileinput');
     require('bootstrap-slider');
     require('jquery-mousewheel');
 
