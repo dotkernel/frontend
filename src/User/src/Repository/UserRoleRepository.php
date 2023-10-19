d8229fa29b56c02d1dce8791b66e4d93f268303b
MarioRadu <magda_marior@yahoo.com>
2023-10-19 14:27:44 +0300

    symfony insights bug fixes

--- src/User/src/Repository/UserRoleRepository.php
+++ src/User/src/Repository/UserRoleRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Frontend\User\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Dot\AnnotatedServices\Annotation\Entity;
 
 /**
  * @Entity(name="Frontend\User\Entity\UserRole")
