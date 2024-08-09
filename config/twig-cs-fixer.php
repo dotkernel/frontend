0f05764e09f3f2cef19288b368f39720d226a358
bidi <bidi@apidemia.com>
2024-08-09 13:52:28 +0300

    updated twig cs config
    Signed-off-by: bidi <bidi@apidemia.com>

--- config/twig-cs-fixer.php
+++ config/twig-cs-fixer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $config = new TwigCsFixer\Config\Config();
 $config->addTwigExtension(new Dot\Twig\Extension\DateExtension());
 $config->addTwigExtension(new Dot\Twig\Extension\TranslationExtension());
