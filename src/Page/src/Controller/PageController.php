b93ecf35c322d423d3b1217714c0f39742ae7491
bidi <bidi@apidemia.com>
2024-08-09 18:15:56 +0300

    added canonical url
    Signed-off-by: bidi <bidi@apidemia.com>

--- src/Page/src/Controller/PageController.php
+++ src/Page/src/Controller/PageController.php
@@ -12,31 +12,37 @@ use Mezzio\Router\RouterInterface;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\ResponseInterface;
 
+use function rtrim;
+
 class PageController extends AbstractActionController
 {
     #[Inject(
         PageServiceInterface::class,
         RouterInterface::class,
         TemplateRendererInterface::class,
+        "config",
     )]
     public function __construct(
         protected PageServiceInterface $pageService,
         protected RouterInterface $router,
-        protected TemplateRendererInterface $template
+        protected TemplateRendererInterface $template,
+        protected array $config = []
     ) {
     }
 
     public function indexAction(): ResponseInterface
     {
+        $canonicalUrl = rtrim($this->config['application']['url'], '/') . $this->router->generateUri('home');
         return new HtmlResponse(
-            $this->template->render('page::home')
+            $this->template->render('page::home', ['canonicalUrl' => $canonicalUrl])
         );
     }
 
     public function homeAction(): ResponseInterface
     {
+        $canonicalUrl = rtrim($this->config['application']['url'], '/') . $this->router->generateUri('home');
         return new HtmlResponse(
-            $this->template->render('page::home')
+            $this->template->render('page::home', ['canonicalUrl' => $canonicalUrl])
         );
     }
 
