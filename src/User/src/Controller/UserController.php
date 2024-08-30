00d553870e000a138a47bab98dcf5d30a5867f77
Claudiu Pintiuta <claudiupintiuta@gmail.com>
2024-08-30 17:48:57 +0300

    changes from review
    Signed-off-by: Claudiu Pintiuta <claudiupintiuta@gmail.com>

--- src/User/src/Controller/UserController.php
+++ src/User/src/Controller/UserController.php
@@ -54,7 +54,7 @@ class UserController extends AbstractActionController
         }
 
         $form = new LoginForm();
-        $form->setAttribute('action', $this->router->generateUri("user", ['action' => 'login']));
+        $form->setAttribute('action', $this->router->generateUri('user', ['action' => 'login']));
 
         $shouldRebind = $this->messenger->getData('shouldRebind') ?? true;
         if ($shouldRebind) {
@@ -124,11 +124,11 @@ class UserController extends AbstractActionController
     public function registerAction(): ResponseInterface
     {
         if ($this->authenticationService->hasIdentity()) {
-            return new RedirectResponse($this->router->generateUri("page"));
+            return new RedirectResponse($this->router->generateUri('page'));
         }
 
         $form = new RegisterForm();
-        $form->setAttribute('action', $this->router->generateUri("user", ['action' => 'register']));
+        $form->setAttribute('action', $this->router->generateUri('user', ['action' => 'register']));
 
         $shouldRebind = $this->messenger->getData('shouldRebind') ?? true;
         if ($shouldRebind) {
