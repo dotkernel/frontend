eab817d8943f93eef93db54858f05b9138a3fd2c
alexmerlin <alex.merlin.1985@gmail.com>
2024-10-01 09:33:56 +0300

    Issue #490: Enabled FastRoute caching
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

---
+++ config/autoload/routes.global.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+$debug = require getcwd() . '/config/autoload/debug.global.php';
+
+return [
+    'router' => [
+        'fastroute' => [
+            Mezzio\Router\FastRouteRouter::CONFIG_CACHE_ENABLED => $debug['_debug'],
+        ],
+    ],
+];
