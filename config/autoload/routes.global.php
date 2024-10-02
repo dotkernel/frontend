d01ce3a90ed65a24ad9cc45f2271c623cf5d15e2
Alex Karajos <alex.merlin.1985@gmail.com>
2024-10-02 09:31:44 +0300

    Update routes.global.php
    Signed-off-by: Alex Karajos <alex.merlin.1985@gmail.com>

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -7,7 +7,7 @@ $debug = require getcwd() . '/config/autoload/debug.global.php';
 return [
     'router' => [
         'fastroute' => [
-            Mezzio\Router\FastRouteRouter::CONFIG_CACHE_ENABLED => $debug['_debug'],
+            Mezzio\Router\FastRouteRouter::CONFIG_CACHE_ENABLED => ! $debug['_debug'],
         ],
     ],
 ];
