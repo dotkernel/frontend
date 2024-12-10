20bb143b39924ae0cb6e8d6320027c35c6c4ccd2
MarioRadu <magda_marior@yahoo.com>
2024-12-10 19:24:06 +0200

    requested changes
    Signed-off-by: MarioRadu <magda_marior@yahoo.com>

--- bin/composer-post-install-script.php
+++ bin/composer-post-install-script.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+const ENVIRONMENT_DEVELOPMENT = 'development';
+const ENVIRONMENT_PRODUCTION  = 'production';
+
 // phpcs:disable PSR1.Files.SideEffects.FoundWithSymbols
 
 function copyFile(array $file): void
@@ -9,7 +12,7 @@ function copyFile(array $file): void
     if (is_readable($file['destination'])) {
         echo "File {$file['destination']} already exists." . PHP_EOL;
     } else {
-        if (! isDevModeEnabled() && $file['environment'] === 'local') {
+        if (! in_array(getEnvironment(), $file['environment'])) {
             echo "Skipping the copy of {$file['source']} due to environment settings." . PHP_EOL;
         } else {
             if (! copy($file['source'], $file['destination'])) {
@@ -21,30 +24,28 @@ function copyFile(array $file): void
     }
 }
 
-function isDevModeEnabled(): bool
+function getEnvironment(): string
 {
-    return file_exists('config/autoload/development.local.php');
+    return file_exists('config/autoload/development.local.php') ? ENVIRONMENT_DEVELOPMENT : ENVIRONMENT_PRODUCTION;
 }
 
-// when adding files to the below array the `source` and `destination` must be relative to the project root folder
-// the `environment` key will indicate when the file should be copied,
-// if the value is `production` the file will be copied on both production and local environments,
-// if the value is `local` the file will be copied only on local environments
+// when adding files to the below array the `source` and `destination` paths must be relative to the project root folder
+// the `environment` key will indicate on what environments the file will be copied,
 $files = [
     [
         'source'      => 'config/autoload/local.php.dist',
         'destination' => 'config/autoload/local.php',
-        'environment' => 'production',
+        'environment' => [ENVIRONMENT_DEVELOPMENT, ENVIRONMENT_PRODUCTION],
     ],
     [
         'source'      => 'config/autoload/local.test.php.dist',
         'destination' => 'config/autoload/local.test.php',
-        'environment' => 'local',
+        'environment' => [ENVIRONMENT_DEVELOPMENT],
     ],
     [
         'source'      => 'vendor/dotkernel/dot-mail/config/mail.global.php.dist',
         'destination' => 'config/autoload/mail.global.php',
-        'environment' => 'production',
+        'environment' => [ENVIRONMENT_DEVELOPMENT, ENVIRONMENT_PRODUCTION],
     ],
 ];
 
